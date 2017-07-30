cb65872757caad6d3b96bc2e213dd739338e1e39
Zealous-w <zealous7@163.com>
2017-07-30 07:54:53 -0700

    fix

--- SceneServer.cc
+++ SceneServer.cc
@@ -78,49 +78,51 @@ void SceneServer::onMessage(const muduo::net::TcpConnectionPtr& conn,
                            muduo::net::Buffer* buf,
                            muduo::Timestamp time)
 {
-    const int32_t msgLen = buf->checkPeekInt32();
-    if ( msgLen > buf->readableBytes() ) {
-        LOG_INFO << "MSG LEN : " << buf->peekInt32() << " buf len : " << buf->readableBytes();
-        return;
-    }
-    
-    struct PACKET pkt(std::string(conn->name().c_str()));
-    pkt.len = buf->checkReadInt32();
-    pkt.cmd = buf->checkReadInt32();
-    pkt.uid = buf->checkReadInt32();
-    //LOG_INFO << "OnMessage msgLen : " << msgLen << " len : " << pkt.len - PACKET_HEAD_LEN;
-    pkt.msg = std::string(buf->retrieveAsString(pkt.len - PACKET_HEAD_LEN).c_str(), pkt.len - PACKET_HEAD_LEN);
+    while (buf->readableBytes() > sizeof(int32_t)) {
+        const int32_t msgLen = buf->checkPeekInt32();
+        if ( msgLen > buf->readableBytes() ) {
+            LOG_INFO << "MSG LEN : " << buf->peekInt32() << " buf len : " << buf->readableBytes();
+            break;
+        }
+        
+        struct PACKET pkt(std::string(conn->name().c_str()));
+        pkt.len = buf->checkReadInt32();
+        pkt.cmd = buf->checkReadInt32();
+        pkt.uid = buf->checkReadInt32();
+        //LOG_INFO << "OnMessage msgLen : " << msgLen << " len : " << pkt.len - PACKET_HEAD_LEN;
+        pkt.msg = std::string(buf->retrieveAsString(pkt.len - PACKET_HEAD_LEN).c_str(), pkt.len - PACKET_HEAD_LEN);
 
-    switch (pkt.cmd)
-    {
-        case uint32(cs::ProtoID::ID_C2S_Ping):
+        switch (pkt.cmd)
         {
-            cs::C2S_Ping ping;
-            if ( !ping.ParseFromString(pkt.msg) )
+            case uint32(cs::ProtoID::ID_C2S_Ping):
             {
-                LOG_ERROR << "Proto ping parse error";
+                cs::C2S_Ping ping;
+                if ( !ping.ParseFromString(pkt.msg) )
+                {
+                    LOG_ERROR << "Proto ping parse error";
+                    return;
+                }
+
+                uint32 send_time = ping.send_time();
+
+                struct timeval tv;
+                gettimeofday(&tv,NULL);
+                uint32 recv_time = tv.tv_sec*1000 + tv.tv_usec/1000;
+
+                cs::S2C_Ping sping;
+                sping.set_send_time(send_time);
+                sping.set_recv_time(recv_time);
+
+                std::string msg;
+                msg = sping.SerializeAsString();
+                std::string smsg = g_World->BuildPacket(0, uint32(cs::ProtoID::ID_S2C_Ping), msg);
+                conn->send(smsg.c_str(), smsg.size());
                 return;
-            }
+            }   
+        }
 
-            uint32 send_time = ping.send_time();
-
-            struct timeval tv;
-            gettimeofday(&tv,NULL);
-            uint32 recv_time = tv.tv_sec*1000 + tv.tv_usec/1000;
-
-            cs::S2C_Ping sping;
-            sping.set_send_time(send_time);
-            sping.set_recv_time(recv_time);
-
-            std::string msg;
-            msg = sping.SerializeAsString();
-            std::string smsg = g_World->BuildPacket(0, uint32(cs::ProtoID::ID_S2C_Ping), msg);
-            conn->send(smsg.c_str(), smsg.size());
-            return;
-        }   
+        g_World->PushMsg(pkt);
     }
-
-    g_World->PushMsg(pkt);
 }
 
 int main(int argc, char* argv[]) 
